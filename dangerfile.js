9e087c4874d04dc7ffc5c40291f310f37d2478c2
Jamfly <cocoredju222@gmail.com>
2019-01-23 23:57:34 +0800

    Change fail to warn for danger reviwer

--- dangerfile.js
+++ dangerfile.js
@@ -21,7 +21,7 @@ const includes = require('lodash.includes')
  */
 const reviewersCount = danger.github.requested_reviewers.users.length
 if (reviewersCount === 0) {
-	fail(`🕵 Whoops, I don't see any reviewers. Remember to add one.`)
+	warn(`🕵 Whoops, I don't see any reviewers. Remember to add one.`)
 } else if (reviewersCount > 1) {
 	warn(
 		`It's great to have ${reviewersCount} reviewers. Remember though that more than 1 reviewer may lead to uncertainty as to who is responsible for the review.`
