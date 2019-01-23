31dc965a950f4e61c0b4af67c9543ed64b5282fe
yaoandy107 <yaoandy107@gmail.com>
2019-01-23 22:53:04 +0800

    Try to fix jest on circleci

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,15 +3,19 @@ import server from '../src/app'
 
 console.log = jest.fn()
 
+afterAll(() => {
+  server.close()
+  console.log('server closed!')
+})
+
 describe('Test HelloWorld', () => {
-  test('It should return status code: 200', async (done) => {
+  test('It should return status code: 200', async () => {
     const response = await request(server).get('/')
     expect(response.statusCode).toBe(200)
-    done()
   })
-  test('It should return body: HelloWorld', async (done) => {
+  test('It should return body: HelloWorld', async () => {
     const response = await request(server).get('/')
     expect(JSON.parse(response.text).data).toBe('Hello World!')
-    done()
   })
+
 })
